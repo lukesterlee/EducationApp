788bf7409e7309c7731297f3e499bde3016605e2
Luke Lee <samsung3star@gmail.com>
2015-08-01 14:42:07 -0400

    jae's code

--- app/src/main/java/hackaccess/c4q/nyc/educationapp/Constants.java
+++ app/src/main/java/hackaccess/c4q/nyc/educationapp/Constants.java
@@ -4,5 +4,6 @@ package hackaccess.c4q.nyc.educationapp;
  * Created by Willee on 8/1/15.
  */
 public abstract class Constants {
-    
+    private static final String TWILIO_NUMBER = "+14065456377";
+
 }
---
+++ app/src/main/java/hackaccess/c4q/nyc/educationapp/LoginActivity.java
@@ -0,0 +1,9 @@
+package hackaccess.c4q.nyc.educationapp;
+
+import android.app.Activity;
+
+/**
+ * Created by Willee on 8/1/15.
+ */
+public class LoginActivity extends Activity {
+}

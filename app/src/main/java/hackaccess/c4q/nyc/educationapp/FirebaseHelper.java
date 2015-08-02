8e73ed9088024945167db6489d2eb8575052172f
AnthonyFermin <AnthonyFerminAFC@gmail.com>
2015-08-02 11:33:04 -0400

    added firebaseHelper database methods

--- app/src/main/java/hackaccess/c4q/nyc/educationapp/FirebaseHelper.java
+++ app/src/main/java/hackaccess/c4q/nyc/educationapp/FirebaseHelper.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.util.Log;
 import android.widget.Toast;
 
-import com.firebase.client.ChildEventListener;
+import com.firebase.client.AuthData;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
@@ -12,11 +12,10 @@ import com.firebase.client.Query;
 import com.firebase.client.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
 
 import hackaccess.c4q.nyc.educationapp.firebase.Like;
-import hackaccess.c4q.nyc.educationapp.firebase.Account;
+import hackaccess.c4q.nyc.educationapp.firebase.UserInfo;
 
 /**
  * Created by c4q-anthonyf on 8/1/15.
@@ -26,6 +25,10 @@ public class FirebaseHelper extends Firebase {
     private static Context sContext;
     private static final String URL = "https://edusearch.firebaseio.com/";
     private static FirebaseHelper INSTANCE;
+    private static String userID;
+    private static UserInfo userInfo;
+    private static ArrayList<Like> userLikes;
+
 
     public FirebaseHelper() {
         super(URL);
@@ -41,42 +44,114 @@ public class FirebaseHelper extends Firebase {
     }
 
 
-    public void createAccount(String emailAddress, String password) {
+    public void createAccount(final String emailAddress, final String password, final UserInfo userInfo) {
+
+
         INSTANCE.createUser(emailAddress, password, new ValueResultHandler<Map<String, Object>>() {
             @Override
             public void onSuccess(Map<String, Object> stringObjectMap) {
+                Toast.makeText(sContext, "Account Created", Toast.LENGTH_SHORT).show();
 
-                Toast.makeText(sContext, "Success!", Toast.LENGTH_SHORT).show();
+                Firebase userRef = INSTANCE.child("users").child(stringObjectMap.get("uid").toString());
+                userRef.setValue(userInfo);
+
+                logInUser(emailAddress, password);
 
             }
 
             @Override
             public void onError(FirebaseError firebaseError) {
-                Toast.makeText(sContext, "Failed!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(sContext, "Email Already in Use", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-    public void createUser(String email, String password){
+    public void logInUser(String emailAddress, String password){
 
-        Account account = new Account(email, password);
+        INSTANCE.authWithPassword(emailAddress, password, new Firebase.AuthResultHandler() {
+            @Override
+            public void onAuthenticated(AuthData authData) {
+                userID = authData.getUid();
+                getUserInfo();
+            }
+            @Override
+            public void onAuthenticationError(FirebaseError firebaseError) {
+                Toast.makeText(sContext, "Email or Password Incorrect", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
-        INSTANCE.child("users").push().setValue(account.getAccountHash());
+    public void logOutUser(){
+        userID = null;
+        userInfo = null;
+    }
 
+    public void changeUserEmail(String oldEmailAddress, String newEmailAddress, String password){
+
+        INSTANCE.changeEmail(oldEmailAddress, newEmailAddress, password, new Firebase.ResultHandler() {
+            @Override
+            public void onSuccess() {
+                Toast.makeText(sContext,"Email Successfully Changed", Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onError(FirebaseError firebaseError) {
+                Toast.makeText(sContext,"Email Change Unsuccessful", Toast.LENGTH_SHORT).show();            }
+        });
+    }
+
+    public UserInfo getUserInfo(){
+        Query userRef = INSTANCE.child("users").equalTo(userID);
+
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                userInfo = (UserInfo) dataSnapshot.getValue();
+                Log.d("UserInfo Retrieved", userInfo.getName());
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+        return userInfo;
     }
 
 
+    public void addLike(String programId, String zipcode){
 
-    public void addLike(long officeId, long userId){
+        if(userID != null){
+
+        }
+
 
     }
 
-    public ArrayList<Like> getOfficeLikes(long officeID){
+    public ArrayList<Like> getOfficeLikes(String programID, String zipcode){
         return null;
     }
 
-    public ArrayList<Like> getUserLikes(Firebase account){
-        return null;
+    public ArrayList<Like> getUserLikes(){
+        if(userID != null) {
+            Query likesRef = INSTANCE.child("userLikes").equalTo(userID);
+            likesRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    userLikes = (ArrayList<Like>) dataSnapshot.getValue();
+                }
+
+                @Override
+                public void onCancelled(FirebaseError firebaseError) {
+
+                }
+            });
+
+            return userLikes;
+        }else{
+            return null;
+        }
+
     }
 
 }
